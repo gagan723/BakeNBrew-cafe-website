32595102bf878183c9ea942116cbe63c482e9a2d
Gagan <gagan01082004@gmail.com>
2025-08-26 20:11:19 +0530

    user modal change

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -7,6 +7,14 @@ const UserSchema = new mongoose.Schema(
     password: { type: String, required: true },
     role: { type: String, default:"user" },
     cartData: { type: Array, default: [] },
+    reservations: [
+      {
+        noOfPeople : {type: Number, required:true},
+        time : {type : String, required:true},
+        date : {type : String, required:true},
+        createdAt: {type: Date, default: Date.now}
+      }
+    ]
   },
   { minimize: false }
 );
